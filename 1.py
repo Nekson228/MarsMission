2fbf6698f965c0433c3ddd7656c6cd484c6174f5
NeePaw <bangerwockeez@gmail.com>
2021-02-26 22:12:38 +0500

    5

--- 1.py
+++ 1.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
 import json
 
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/<title>')
@@ -37,6 +42,27 @@ def answer():
         'ready': False,
     }
     return render_template('answer.html', **data)
+
+
+class DoubleProtectionForm(FlaskForm):
+    astronaut_id = StringField('ID Астронавта', validators=[DataRequired()])
+    astronaut_password = PasswordField('Пароль Астронавта', validators=[DataRequired()])
+    captain_id = StringField('ID Капитана', validators=[DataRequired()])
+    captain_password = PasswordField('Пароль Капитана', validators=[DataRequired()])
+    submit = SubmitField('Получить доступ')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = DoubleProtectionForm()
+    if form.validate_on_submit():
+        return redirect('/login/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/login/success')
+def success():
+    return render_template('success.html')
 
 
 if __name__ == '__main__':
